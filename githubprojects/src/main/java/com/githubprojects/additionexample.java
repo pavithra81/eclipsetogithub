73f444d6792556fcef93903bf509ab5eb460fac0
pavit <pavit@Pavithra>
2022-04-21 10:34:29 +0530

    basic eclipse project

---
+++ githubprojects/src/main/java/com/githubprojects/additionexample.java
@@ -0,0 +1,14 @@
+package com.githubprojects;
+
+public class additionexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a=10;
+b=9;
+c=a+b;
+System.out.println(c);
+	}
+
+}
---
+++ githubprojects/src/main/java/com/githubprojects/helloworld.java
@@ -0,0 +1,11 @@
+package com.githubprojects;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hi i am from udupi");
+        System.out.println("i am learning java");
+	}
+
+}
